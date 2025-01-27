2e20f3bfd5513b7ec71053c8c2b28c66684f9e5e
maaru <maratarzymatov288@gmail.com>
2025-01-27 15:48:31 +0600

    4, 5 домашка 😄

--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -1,16 +1,14 @@
-from .models import Movie, Review, Director
 from rest_framework import serializers
+from .models import Movie, Review, Director
 
 
 class DirectorSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=100)
     movie_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Director
-        fields = [
-            "name",
-            "movie_count",
-        ]
+        fields = ("name", "movie_count")
 
     def get_movie_count(self, director):
         return director.movies.count()
@@ -19,33 +17,42 @@ class DirectorSerializer(serializers.ModelSerializer):
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = [
-            "text",
-            "movie",
-            "stars",
-        ]
+        fields = ("id", "text", "movie", "stars")
+
+
+class ReviewValiditySerializer(serializers.Serializer):
+    text = serializers.CharField()
+    movie = serializers.IntegerField()
+    stars = serializers.IntegerField(min_value=1, max_value=5)
 
 
 class MovieSerializer(serializers.ModelSerializer):
     director = DirectorSerializer()
     reviews = ReviewSerializer(many=True)
-    everage_rating = serializers.SerializerMethodField()
+    average_rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Movie
-        fields = [
+        fields = (
             "title",
             "description",
             "duration",
             "director",
             "reviews",
-            "everage_rating",
-        ]
+            "average_rating",
+        )
 
-    def get_everage_rating(self, movie):
+    def get_average_rating(self, movie):
         reviews = movie.reviews.all()
         if reviews:
             sum_reviews = sum([review.stars for review in reviews])  # 6
-            average = sum_reviews / len(reviews)  # 6 / 2 = 3
+            average = sum_reviews / len(reviews)  # 6/2 = 3
             return average
         return None
+
+
+class MovieValidirySerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    duration = serializers.IntegerField()
+    director = serializers.IntegerField()
--- movie_app/urls.py
+++ movie_app/urls.py
@@ -1,20 +1,18 @@
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("director/", views.DirectorListAPIView.as_view(), name="director-list"),
+    path("directors/", views.DirectorListAPIView.as_view(), name="director-list"),
     path(
-        "director/<int:pk>",
-        views.DirectroDetailAPIView.as_view(),
+        "directors/<int:id>/",
+        views.DirectorDetailAPIView.as_view(),
         name="director-detail",
     ),
-    path("movie/", views.MovieListAPIView.as_view(), name="movie-list"),
-    path("movie/<int:pk>", views.MovieDetailAPIView.as_view(), name="movie-detail"),
-    path("review/", views.ReviewListAPIView.as_view(), name="review-list"),
+    path("movies/", views.MovieListAPIView.as_view(), name="movie-list"),
+    path("movies/<int:id>/", views.MovieDetailAPIView.as_view(), name="movie-detail"),
+    path("reviews/", views.ReviewListAPIView.as_view(), name="review-list"),
     path(
-        "review/<int:pk>",
-        views.ReviewDetailAPIView.as_view(),
-        name="review-detail",
+        "reviews/<int:id>/", views.ReviewDetailAPIView.as_view(), name="review-detail"
     ),
 ]
